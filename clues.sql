612e943ce8cd380a56bbadc919ab886ff0a45d5b
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-16 15:11:21 -0600

    adjusting line breaks and spacing

--- clues.sql
+++ clues.sql
@@ -30,29 +30,17 @@ HAVING COUNT(*) = 1 AND MAX(countrylanguages.language) = 'Italian';
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
-SELECT 
-    cities.name,  
-    cities.population 
-FROM 
-    cities 
-JOIN 
-    countries ON cities.countrycode = countries.code 
-WHERE 
-    countries.name = 'San Marino';
+SELECT cities.name, cities.population 
+FROM cities 
+JOIN countries ON cities.countrycode = countries.code 
+WHERE countries.name = 'San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-SELECT 
-  cities.name, 
-  countries.name
- 
-FROM 
-  cities
-JOIN 
-  countries ON cities.countrycode = countries.code
-WHERE 
-  countries.continent = 'South America'
-  AND cities.name LIKE 'Ser%';
+SELECT cities.name, countries.name
+FROM cities
+JOIN countries ON cities.countrycode = countries.code
+WHERE countries.continent = 'South America' AND cities.name LIKE 'Ser%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -61,15 +49,10 @@ WHERE
 
 -- Write SQL query here
 
-SELECT 
-    cities.name
-FROM
-    cities
-JOIN
-    countries ON cities.countrycode = countries.code
-WHERE
-    countries.capital = cities.id
-    AND countries.name = 'Brazil';
+SELECT cities.name
+FROM cities
+JOIN countries ON cities.countrycode = countries.code
+WHERE countries.capital = cities.id AND countries.name = 'Brazil';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -85,11 +68,7 @@ WHERE
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-SELECT 
-    cities.name, countries.name
-FROM
-    cities
-JOIN
-    countries ON cities.countrycode = countries.code
-WHERE
-    cities.population = 91084;
+SELECT cities.name, countries.name
+FROM cities
+JOIN countries ON cities.countrycode = countries.code
+WHERE cities.population = 91084;
